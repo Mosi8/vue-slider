214ade572e6a525320d6f4d6f8453a00290ed22a
Mosi8 <mauriziomosi8@gmail.com>
2022-01-14 16:41:27 +0100

    completo senza bonus

--- myscript.js
+++ myscript.js
@@ -1,199 +1,62 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-];
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
-
-
-// ciclo for immagini piccole 
-let itemsPiccole = '';
-let fotoActive = 0;
-
-for (let i = 0; i < items.length; i++) {
-    itemsPiccole += `
-    <div class="col-12 p-0 ms_h20 ms_oscura" id="items-${i}">
-        <img src="${items[i]}">
-    </div>`;
-};
-
-const imgPiccole = document.getElementById('img_piccole');
-imgPiccole.innerHTML = itemsPiccole;
-
-let fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
-fotoPiccolaActive.classList.remove('ms_oscura');
-fotoPiccolaActive.classList.add('ms_no_oscura');
-fotoPiccolaActive.classList.add('ms_border');
-
-
-// ciclo for immagini grande 
-let itemGrande = '';
-let elementActive = 0;
-
-for (let i = 0; i < items.length; i++) {
-    itemGrande += `
-    <img class="hidden" id="item-grande-${i}" src="${items[i]}">
-    <div class="ms_posAbs">
-        <div class="hidden text-end ms_title" id="title-img-${i}">${title[i]}</div>
-        <div class="hidden text-end ms_text" id="text-img-${i}">${text[i]}</div>
-    </div>`;
-};
-
-const imgGrande = document.getElementById('img_grande');
-imgGrande.innerHTML = itemGrande;
-
-let itemActive = document.getElementById('item-grande-'+ elementActive);
-itemActive.classList.remove('hidden');
-itemActive.classList.add('active');
-
-let titleActive = document.getElementById('title-img-'+ elementActive);
-titleActive.classList.remove('hidden');
-titleActive.classList.add('active');
-
-let textActive = document.getElementById('text-img-'+ elementActive);
-textActive.classList.remove('hidden');
-textActive.classList.add('active');
-
-// tasti up e down 
-const up = document.querySelector('.up');
-const down = document.querySelector('.down');
-
-// scendere immagine 
-down.addEventListener('click', function() {
-    fotoActive++;
-    elementActive++;
-    // foto grande 
-    if (elementActive == items.length) {
-        elementActive = 0;
-        itemActive.classList.add('hidden');
-        itemActive.classList.remove('active');
-        itemActive = document.getElementById('item-grande-'+ elementActive);
-        itemActive.classList.remove('hidden');
-        itemActive.classList.add('active');
-        titleActive.classList.add('hidden');
-        titleActive.classList.remove('active');
-        titleActive = document.getElementById('title-img-'+ elementActive);
-        titleActive.classList.remove('hidden');
-        titleActive.classList.add('active');
-        textActive.classList.add('hidden');
-        textActive.classList.remove('active');
-        textActive = document.getElementById('text-img-'+ elementActive);
-        textActive.classList.remove('hidden');
-        textActive.classList.add('active');
-    }else{
-        itemActive.classList.add('hidden');
-        itemActive.classList.remove('active');
-        itemActive = document.getElementById('item-grande-'+ elementActive);
-        itemActive.classList.remove('hidden');
-        itemActive.classList.add('active');
-        titleActive.classList.add('hidden');
-        titleActive.classList.remove('active');
-        titleActive = document.getElementById('title-img-'+ elementActive);
-        titleActive.classList.remove('hidden');
-        titleActive.classList.add('active');
-        textActive.classList.add('hidden');
-        textActive.classList.remove('active');
-        textActive = document.getElementById('text-img-'+ elementActive);
-        textActive.classList.remove('hidden');
-        textActive.classList.add('active');
+let app = new Vue ({
+    el : '#magia',
+    data : {
+        visualizzata : 0,
+        cartoline : [
+            {
+                'immagine' : 'img/01.jpg',
+                'luogo' : 'Svezia',
+                'testo' : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                'immagine' : 'img/02.jpg',
+                'luogo' : 'Sviezzera',
+                'testo' : 'Lorem ipsum',
+            },
+            {
+                'immagine' : 'img/03.jpg',
+                'luogo' : 'Gran Bretagna',
+                'testo' : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                'immagine' : 'img/04.jpg',
+                'luogo' : 'Germania',
+                'testo' : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                'immagine' : 'img/05.jpg',
+                'luogo' : 'Paradise',
+                'testo' : 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        
+        ],
+    },
+    methods : {
+        mostra : function (indice) {
+            if(indice == this.visualizzata){
+                return 'active'
+            }
+            return 'hidden'
+        },
+        selezionata : function (indice) {
+            if(indice == this.visualizzata){
+                return 'ms_no_oscura','ms_border'
+            }
+            return 'ms_oscura'
+        },
+        successiva : function () {
+            this.visualizzata++;
+            if(this.visualizzata == this.cartoline.length){
+                this.visualizzata = 0
+            };
+        },
+        precedente : function () {
+            this.visualizzata--;
+            if(this.visualizzata < 0){
+                this.visualizzata = this.cartoline.length - 1;
+            };
+        },
+        
     }
-
-    // foto piccola 
-    if (fotoActive == items.length) {
-        fotoActive = 0;
-        fotoPiccolaActive.classList.add('ms_oscura');
-        fotoPiccolaActive.classList.remove('ms_no_oscura');
-        fotoPiccolaActive.classList.remove('ms_border');
-        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
-        fotoPiccolaActive.classList.remove('ms_oscura');
-        fotoPiccolaActive.classList.add('ms_no_oscura');
-        fotoPiccolaActive.classList.add('ms_border');
-    }else{
-        fotoPiccolaActive.classList.add('ms_oscura');
-        fotoPiccolaActive.classList.remove('ms_no_oscura');
-        fotoPiccolaActive.classList.remove('ms_border');
-        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
-        fotoPiccolaActive.classList.remove('ms_oscura');
-        fotoPiccolaActive.classList.add('ms_no_oscura');
-        fotoPiccolaActive.classList.add('ms_border'); 
-    }
-});
-
-
-// salire immagine 
-up.addEventListener('click', function() {
-    fotoActive--;
-    elementActive--;
-    // foto grande 
-    if (elementActive == 0-1) {
-        elementActive = items.length-1;
-        itemActive.classList.add('hidden');
-        itemActive.classList.remove('active');
-        itemActive = document.getElementById('item-grande-'+ elementActive);
-        itemActive.classList.remove('hidden');
-        itemActive.classList.add('active');
-        titleActive.classList.add('hidden');
-        titleActive.classList.remove('active');
-        titleActive = document.getElementById('title-img-'+ elementActive);
-        titleActive.classList.remove('hidden');
-        titleActive.classList.add('active');
-        textActive.classList.add('hidden');
-        textActive.classList.remove('active');
-        textActive = document.getElementById('text-img-'+ elementActive);
-        textActive.classList.remove('hidden');
-        textActive.classList.add('active');
-    }else{
-        itemActive.classList.add('hidden');
-        itemActive.classList.remove('active');
-        itemActive = document.getElementById('item-grande-'+ elementActive);
-        itemActive.classList.remove('hidden');
-        itemActive.classList.add('active');
-        titleActive.classList.add('hidden');
-        titleActive.classList.remove('active');
-        titleActive = document.getElementById('title-img-'+ elementActive);
-        titleActive.classList.remove('hidden');
-        titleActive.classList.add('active');
-        textActive.classList.add('hidden');
-        textActive.classList.remove('active');
-        textActive = document.getElementById('text-img-'+ elementActive);
-        textActive.classList.remove('hidden');
-        textActive.classList.add('active');
-    }
-
-    // foto piccola 
-    if (fotoActive == 0 - 1) {
-        fotoActive = items.length-1;
-        fotoPiccolaActive.classList.add('ms_oscura');
-        fotoPiccolaActive.classList.remove('ms_no_oscura');
-        fotoPiccolaActive.classList.remove('ms_border');
-        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
-        fotoPiccolaActive.classList.remove('ms_oscura');
-        fotoPiccolaActive.classList.add('ms_no_oscura');
-        fotoPiccolaActive.classList.add('ms_border');
-    }else{
-        fotoPiccolaActive.classList.add('ms_oscura');
-        fotoPiccolaActive.classList.remove('ms_no_oscura');
-        fotoPiccolaActive.classList.remove('ms_border');
-        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
-        fotoPiccolaActive.classList.remove('ms_oscura');
-        fotoPiccolaActive.classList.add('ms_no_oscura');
-        fotoPiccolaActive.classList.add('ms_border');
-    }
-});
+}
+);
